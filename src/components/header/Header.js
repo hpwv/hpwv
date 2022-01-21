a509dcaadc286345f7a1ad97b53aca945c35ef12
Maximilian Büttner <maximilian.buettner@westernacher.com>
2022-01-21 18:07:45 +0100

    make heaader a functioncal component;

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,9 @@
-import {Component} from 'react';
 import './Header.css';
 
-export class Header extends Component {
-    render() {
-        return (
-            <div className="Header">
-                <h2>HPWV - High-Performance-Web-Viewer</h2>
-            </div>
-        );
-    }
+export function Header() {
+    return (
+        <div className="Header">
+            <h2>HPWV - High-Performance-Web-Viewer</h2>
+        </div>
+    );
 }
